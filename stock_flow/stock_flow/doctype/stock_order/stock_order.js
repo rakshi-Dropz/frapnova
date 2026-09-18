857139de8b4427306f4e6dd4025ee9577a4d7c9b
Rakshi <rakshitha@dropz.in>
2026-09-18 17:45:20 +0530

    feat(desk): add custom form script with approval action button for Stock Order

--- stock_flow/stock_flow/doctype/stock_order/stock_order.js
+++ stock_flow/stock_flow/doctype/stock_order/stock_order.js
@@ -6,3 +6,27 @@
 
 // 	},
 // });
+frappe.ui.form.on('Stock Order', {
+    refresh: function(frm) {
+        if (frm.doc.docstatus === 0 && frappe.user_roles.includes("Stock Manager")) {
+            frm.add_custom_button(__('Approve Order'), function() {
+                frappe.call({
+                    method: 'stock_flow.api.approve_stock_order',
+                    args: { order_id: frm.doc.name },
+                    callback: function(r) {
+                        if (!r.exc) {
+                            frappe.msgprint(__('Order approved successfully.'));
+                            frm.reload_doc();
+                        }
+                    }
+                });
+            }).addClass('btn-success');
+        }
+    },
+
+    quantity: function(frm) {
+        if (frm.doc.quantity && frm.doc.rate) {
+            frm.set_value('total_amount', frm.doc.quantity * frm.doc.rate);
+        }
+    }
+});
